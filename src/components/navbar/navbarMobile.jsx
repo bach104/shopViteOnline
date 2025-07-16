7298d2908c18ada141f56294ca11a5894370eb78
bach104 <havanhung10a2@gmail.com>
2025-07-16 08:44:38 +0700

    upadte

--- src/components/navbar/navbarMobile.jsx
+++ src/components/navbar/navbarMobile.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 const navbarMobile = () => {
   return (
-    <div>
+    <div className>
         <nav className="cursor-pointer text-2xl">
             <FontAwesomeIcon icon={faBars} />
         </nav>
