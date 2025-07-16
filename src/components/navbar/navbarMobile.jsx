a228d4c846cbfbe11a686a6c211c8ce71352f3d7
bach104 <havanhung10a2@gmail.com>
2025-07-16 10:58:08 +0700

    upadte

--- src/components/navbar/navbarMobile.jsx
+++ src/components/navbar/navbarMobile.jsx
@@ -1,11 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 const navbarMobile = () => {
   return (
-    <div className>
+    <div className="relative navbarMobile__menu">
         <nav className="cursor-pointer text-2xl">
             <FontAwesomeIcon icon={faBars} />
         </nav>
+        <ul className="absolute navbarMobile__list bg-white left-0 top:0">
+            <li>
+                <Link to="/">Trang chủ</Link>
+            </li>
+            <li>
+                <Link to="">Cửa hàng</Link>
+            </li>
+            <li>
+                <Link to="">Sản phẩm</Link>
+            </li>
+            <li>
+                <Link to="">Giới thiệu</Link>
+            </li>
+        </ul>
     </div>
   )
 }
